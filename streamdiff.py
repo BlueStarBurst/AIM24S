bea688fe872df11376ad6a654c3e069e75dbe4c6
bluestarburst <hargreaves.bryant@gmail.com>
2024-04-30 09:27:39 -0500

    shape

--- streamdiff.py
+++ streamdiff.py
@@ -95,8 +95,9 @@ def streamdiffusion(image, mask=None):
     
     maskArr = np.array(mask)
     print(maskArr.shape)
-    # make 3 channels (512, 512) -> (512, 512, 3)
-    maskArr = cv2.cvtColor(maskArr, cv2.COLOR_GRAY2RGB)
+    # make 3 channels (512, 512) -> (512, 512, 3) with numpy
+    maskArr = np.stack((maskArr,)*3, axis=-1)
+    print(maskArr.shape)
     
     blurred_mask_image = cv2.blur(np.array(maskArr), (35,35))
     
