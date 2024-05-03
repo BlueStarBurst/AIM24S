749a126614db7a92fe673b3955428f2038fd0369
bluestarburst <hargreaves.bryant@gmail.com>
2024-05-03 14:59:17 -0500

    diff params

--- server.py
+++ server.py
@@ -222,7 +222,7 @@ def receiveText():
                 
             if newTextPrompt != textPrompt:
                 print("Changing prompt to:", newTextPrompt)
-                setprompt(newTextPrompt, "bad quality, fake, not realistic")
+                setprompt(newTextPrompt + ", high definition, colorful, realistic", "bad quality, fake, not realistic, grayscale, black and white, not colorful, blurry")
                 textPrompt = newTextPrompt
                 
             tempData = nextTempData
